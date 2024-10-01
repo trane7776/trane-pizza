ee9b15c672f55acd074aa8215c4af7694c2b1e91
trane7776 <izbasarov.t@edu.rea.ru>
2024-10-01 23:39:56 +0300

    adding change pizza variants

--- components/shared/ChoosePizzaForm.tsx
+++ components/shared/ChoosePizzaForm.tsx
@@ -1,7 +1,8 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
 import { Button } from '../ui';
-import { PizzaImage, Title } from '.';
+import { GroupVariants, PizzaImage, Title } from '.';
+import { PizzaSize, pizzaSizes, PizzaType } from '@/constants/pizza';
 interface Props {
   imageUrl: string;
   name: string;
@@ -19,15 +20,24 @@ export const ChoosePizzaForm: React.FC<Props> = ({
   onClickAdd,
   className,
 }) => {
+  const [size, setSize] = React.useState<PizzaSize>(20);
+  const [type, setType] = React.useState<PizzaType>(1);
+
   const textDetails = '30 см, традиционное тесто 30';
   const totalPrice = 1000;
-  const size = 30;
+
   return (
     <div className={cn(className, 'flex flex-1')}>
       <PizzaImage imageUrl={imageUrl} size={size} />
       <div className="w-[490px] bg-[#fcfcfc] p-7">
         <Title text={name} size="md" className="font-extrabold mb-1" />
         <p className="text-gray-400">{textDetails}</p>
+
+        <GroupVariants
+          items={pizzaSizes}
+          selectedValue={String(size)}
+          onClick={(value) => setSize(Number(value) as PizzaSize)}
+        />
         <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
           добавить в корзину за {totalPrice} ₽
         </Button>
---
+++ constants/pizza.ts
@@ -0,0 +1,24 @@
+export const mapPizzaSize = {
+  20: 'маленькая',
+  30: 'средняя',
+  40: 'большая',
+} as const;
+
+export const mapPizzaType = {
+  1: 'традиционное',
+  2: 'тонкое',
+} as const;
+
+export const pizzaSizes = Object.entries(mapPizzaSize).map(([value, name]) => ({
+  name,
+  value,
+}));
+
+// [
+//   { name: 20, value: 'маленькая' },
+//   { name: 30, value: 'средняя' },
+//   { name: 40, value: 'большая' }
+// ]
+
+export type PizzaSize = keyof typeof mapPizzaSize;
+export type PizzaType = keyof typeof mapPizzaType;
