d001e91d469ac965782ad4ebf011ef6f17f7b7ad
trane7776 <izbasarov.t@edu.rea.ru>
2024-10-03 23:39:28 +0300

    check for existing pizza variants

--- components/shared/ChoosePizzaForm.tsx
+++ components/shared/ChoosePizzaForm.tsx
@@ -4,6 +4,7 @@ import { Button } from '../ui';
 import { GroupVariants, PizzaImage, Title, IngredientItem } from '.';
 import { Ingredient, ProductItem } from '@prisma/client';
 import {
+  mapPizzaType,
   PizzaSize,
   pizzaSizes,
   PizzaType,
@@ -35,7 +36,6 @@ export const ChoosePizzaForm: React.FC<Props> = ({
     new Set<number>([])
   );
 
-  const textDetails = '30 см, традиционное тесто 30';
   const pizzaPrice =
     items.find((item) => item.pizzaType === type && item.size === size)
       ?.price || 0;
@@ -46,6 +46,26 @@ export const ChoosePizzaForm: React.FC<Props> = ({
       .reduce((acc, ingredient) => acc + ingredient.price, 0) || 0;
 
   const totalPrice = pizzaPrice + totalIngredientsPrice;
+
+  const textDetails = `${size} см, ${mapPizzaType[type]} тесто`;
+
+  const handleClickAdd = () => {
+    onClickAddCart?.();
+    console.log({
+      size,
+      type,
+      ingredients: selectedIngredients,
+    });
+  };
+
+  const availablePizzas = items.filter((item) => item.pizzaType === type);
+  const availablePizzaSizes = pizzaSizes.map((item) => ({
+    name: item.name,
+    value: item.value,
+    disabled: !availablePizzas.some(
+      (pizza) => Number(pizza.size) === Number(item.value)
+    ),
+  }));
   return (
     <div className={cn(className, 'flex flex-1')}>
       <PizzaImage imageUrl={imageUrl} size={size} />
@@ -55,7 +75,7 @@ export const ChoosePizzaForm: React.FC<Props> = ({
 
         <div className="flex flex-col gap-4 mt-5">
           <GroupVariants
-            items={pizzaSizes}
+            items={availablePizzaSizes}
             selectedValue={String(size)}
             onClick={(value) => setSize(Number(value) as PizzaSize)}
           />
@@ -82,7 +102,10 @@ export const ChoosePizzaForm: React.FC<Props> = ({
           </div>
         </div>
 
-        <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
+        <Button
+          onClick={handleClickAdd}
+          className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10"
+        >
           добавить в корзину за {totalPrice} ₽
         </Button>
       </div>
