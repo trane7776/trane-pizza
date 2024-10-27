c15f9881d2a4a8f3bfffa5374f1e7967a4fdc8e2
trane7776 <izbasarov.t@edu.rea.ru>
2024-10-27 17:45:30 +0300

    feat: added insta stories for pizza feed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,8 +9,8 @@ import { UserRole } from '@prisma/client';
 export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
     GitHubProvider({
       clientId: process.env.GITHUB_ID || '',
